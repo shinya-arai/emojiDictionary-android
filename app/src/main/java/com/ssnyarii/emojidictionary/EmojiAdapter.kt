20da2f0a3042dd3f23bd12bf162d68cfaefd5cb8
Arai Shinya <>
2019-06-13 23:08:23 +0900

    first commit

---
+++ app/src/main/java/com/ssnyarii/emojidictionary/EmojiAdapter.kt
@@ -0,0 +1,26 @@
+package com.ssnyarii.emojidictionary
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class EmojiAdapter : RecyclerView.Adapter<EmojiAdapter.TextHolder> {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): TextHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: TextHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class TextHolder(v: View): RecyclerView.ViewHolder(v), View.OnClickListener {
+        override fun onClick(v: View?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
+}
---
+++ app/src/main/java/com/ssnyarii/emojidictionary/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.ssnyarii.emojidictionary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var layoutManager : LinearLayoutManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+
+    }
+}
