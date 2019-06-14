d30178b8960b24934a580323544b0d41718dc017
Arai Shinya <>
2019-06-14 21:47:15 +0900

    recyclerViewあまり理解できないなあ

--- app/src/main/java/com/ssnyarii/emojidictionary/EmojiAdapter.kt
+++ app/src/main/java/com/ssnyarii/emojidictionary/EmojiAdapter.kt
@@ -1,25 +1,45 @@
 package com.ssnyarii.emojidictionary
 
+import android.content.Intent
+import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.recyclerview_item_row.view.*
 
-class EmojiAdapter : RecyclerView.Adapter<EmojiAdapter.TextHolder> {
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): TextHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+class EmojiAdapter(val emojis: ArrayList<String>) : RecyclerView.Adapter<EmojiAdapter.TextHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): TextHolder {
+        return TextHolder(LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item_row, parent, false))
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return emojis.count()
     }
 
-    override fun onBindViewHolder(p0: TextHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: EmojiAdapter.TextHolder, position: Int) {
+        val emoji = emojis[position]
+        holder.bindEmoji(emoji)
     }
 
     class TextHolder(v: View): RecyclerView.ViewHolder(v), View.OnClickListener {
+
+        var view: View = v
+        var emoji: String = ""
+
+        init {
+            v.setOnClickListener(this)
+        }
+
+        fun bindEmoji(emoji: String) {
+            this.emoji = emoji
+            view.itemTextView.text = emoji
+        }
+
         override fun onClick(v: View?) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            val detailIntent = Intent(view.context, EmojiDetailActivity::class.java)
+            detailIntent.putExtra("emoji", emoji)
+            startActivity(view.context, detailIntent, null)
         }
 
     }
